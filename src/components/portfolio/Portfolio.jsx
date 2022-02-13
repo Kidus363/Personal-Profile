e7002e0ee4b6b4543204cc8e70b1f60a5c488798
Kidus363 <59210866+Kidus363@users.noreply.github.com>
2022-02-13 21:33:21 +0300

    added portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,21 +1,64 @@
+import PortfolioList from '../Portfolio.jsx/PortfolioList';
+import { useEffect, useState } from "react";
 import React from './portfolio.scss';
 
 export default function portfolio() {
+  
+  const [selected, setSelected] = useState("featured");
+
+  const list=[
+    {
+      id: 'featured',
+      title: 'Featured'
+    },
+    {
+      id: 'web',
+      title: 'Web Development'
+    },
+    {
+      id: 'work',
+      title: 'Software Engineer'
+    },
+    {
+      id: 'photography',
+      title: 'Photography'
+    },
+  ]
   return(
       <div className="portfolio" id='portfolio'>
         <h1>Portfolio</h1>
         <ul>
-          <li>Website</li>
-          <li>System</li>
-          <li>Apis</li>
-          <li>Photography</li>
-          <li>Others</li>
+          {list.map(item=>(
+            <PortfolioList 
+            title={item.title} 
+            active={selected == item.id} 
+            setSelected={setSelected}
+            id={item.id}
+            />
+          ))}
         </ul>
         <div className="container">
           <div className="item">
-            <img src="" alt="" />
+            <img src="assets/capture.png"  alt="" />
             <h3>Project Management Sytem</h3>
-            </div>          
+          </div>   
+          <div className="item">
+            <img src="assets/capture.png"  alt="" />
+            <h3>Project Management Sytem</h3>
+          </div>   
+          <div className="item">
+            <img src="assets/capture.png"  alt="" />
+            <h3>Project Management Sytem</h3>
+          </div>   
+          <div className="item">
+            <img src="assets/capture.png"  alt="" />
+            <h3>Project Management Sytem</h3>
+          </div>   
+          <div className="item">
+            <img src="assets/capture.png"  alt="" />
+            <h3>Project Management Sytem</h3>
+          </div>          
+
         </div>
       </div>
   ) 
